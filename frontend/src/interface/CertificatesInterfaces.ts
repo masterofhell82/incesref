d604f77ef14c479f1acc8e27ab9e487bea81395a
Andres Benitez <ingabenitez04@gmail.com>
2026-05-10 22:39:40 -0400

    feat: agregar la interfaz CertifiedStudent y actualizar la estructura de CoursesCertificate y Certificate

--- frontend/src/interface/CertificatesInterfaces.ts
+++ frontend/src/interface/CertificatesInterfaces.ts
@@ -1,21 +1,45 @@
+export interface CertifiedStudent {
+  consecutivo?: number;
+  nacionalidad: string;
+  cedula: string;
+  nombres: string;
+  apellidos: string;
+  genero: string;
+  fechaNace: string;
+  telefono: string;
+  correo: string;
+  tituloAsociado: string;
+}
+
 export interface CoursesCertificate {
-    id: number;
-    preimpreso: string;
-    curso: string;
-    participantes: number;
-    fecha_inicio: string;
-    fecha_fin: string;
+  id: number;
+  preimpreso: string;
+  curso: string;
+  participantes: number;
+  fecha_inicio: string;
+  fecha_fin: string;
+  id_estado: number | null;
+  id_cfs: number | null;
+  shortname: string;
+  id_ambito: number | null;
 }
 
 export interface Certificate {
-    id: number;
-    cedula: string;
-    nombres: string;
-    apellidos: string;
-    modalidad: string;
-    certificateId: string;
-    pre_comentario: string;
-    svg_cara_a: string;
-    svg_cara_b: string;
-    is_vigente: boolean;
-  }
+  id: number;
+  nacionalidad: string;
+  cedula: string;
+  nombres: string;
+  apellidos: string;
+  modalidad: string;
+  consecutivo: string;
+  certificateId: string;
+  pre_comentario: string;
+  svg_cara_a: string;
+  svg_cara_b: string;
+  is_vigente: boolean;
+}
+
+export interface SelectOption {
+  value: number;
+  label: string;
+}
