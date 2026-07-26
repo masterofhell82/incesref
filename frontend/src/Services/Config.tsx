4cf3e2710a18304aadf944f1c0e9540de34247b9
Andres Benitez <ingabenitez04@gmail.com>
2026-07-25 20:46:40 -0400

    feat: actualizar configuración de API para usar variable de entorno

--- frontend/src/Services/Config.tsx
+++ frontend/src/Services/Config.tsx
@@ -2,8 +2,8 @@
  * Clase para configurar a donde se debe conectar.
  */
 class Config {
-    static DOMAIN_API = 'http://localhost:3030/api';
-    static API_URL = `${this.DOMAIN_API}`;
+  static DOMAIN_API = process.env.NEXT_PUBLIC_URL_API_BACKEND || 'http://localhost:3030';
+  static API_URL = `${this.DOMAIN_API}/api`;
 }
 
 export default Config;
