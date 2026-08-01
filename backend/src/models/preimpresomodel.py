2f08ccaf6e49a4f4cc4399269e72d08572a07b10
Andres Benitez <ingabenitez04@gmail.com>
2026-07-31 22:07:19 -0400

    feat: agregar campos hoja y libro al modelo PreImpreso y actualizar constructor y método de serialización

--- backend/src/models/preimpresomodel.py
+++ backend/src/models/preimpresomodel.py
@@ -12,15 +12,19 @@ class PreImpresoModel(db.Model):
     codigo = db.Column(db.String(254), nullable=False)
     id_curso = db.Column(db.Integer, nullable=False)
     id_meta = db.Column(db.Integer, nullable=True)
+    hoja = db.Column(db.String(5), nullable=True)
+    libro = db.Column(db.String(4), nullable=True)
     created_at = db.Column(
         db.DateTime, default=datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 
-    def __init__(self, id_curso_activo, preimpreso, codigo, id_curso, id_meta=None):
+    def __init__(self, id_curso_activo, preimpreso, codigo, id_curso, id_meta=None, hoja=None, libro=None):
         self.id_curso_activo = id_curso_activo
         self.preimpreso = preimpreso
         self.codigo = codigo
         self.id_curso = id_curso
         self.id_meta = id_meta
+        self.hoja = hoja
+        self.libro = libro
 
     def serialize(self):
         return {
@@ -30,6 +34,8 @@ class PreImpresoModel(db.Model):
             'codigo': self.codigo,
             'id_curso': self.id_curso,
             'id_meta': self.id_meta,
+            'hoja': self.hoja,
+            'libro': self.libro,
             'created_at': self.created_at
         }
 
